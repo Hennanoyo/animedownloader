c6b2dcea5f272228454fc0076b351b7f0bd4c096
Hennano <hennanoyo@gmail.com>
2026-09-24 06:39:43 +0900

    refactor: use temporary staging for storage adapters

--- server/apps/worker/src/animedownloader_worker/media_attachment_processing.py
+++ server/apps/worker/src/animedownloader_worker/media_attachment_processing.py
@@ -183,12 +183,10 @@ class MediaAttachmentProcessingRunner:
         *,
         state: MediaAttachmentProcessingStateProtocol,
         processor: FFmpegAttachmentProcessor,
-        media_root: Path,
         storage: Storage,
     ) -> None:
         self._state = state
         self._processor = processor
-        self._media_root = media_root
         self._storage = storage
 
     async def run(self, asset_id: UUID) -> None:

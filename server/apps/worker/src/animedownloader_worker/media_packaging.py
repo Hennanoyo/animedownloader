24667127b67362350ba3cafac0587f9ec5608fc7
Hennano <hennanoyo@gmail.com>
2026-09-24 06:39:46 +0900

    refactor: use temporary staging for storage adapters

--- server/apps/worker/src/animedownloader_worker/media_packaging.py
+++ server/apps/worker/src/animedownloader_worker/media_packaging.py
@@ -168,12 +168,10 @@ class MediaPackagingRunner:
         *,
         state: MediaPackagingStateProtocol,
         processor: CMAFPackagingProcessor,
-        media_root: Path,
         storage: Storage,
     ) -> None:
         self._state = state
         self._processor = processor
-        self._media_root = media_root
         self._storage = storage
 
     async def run(self, job_id: UUID) -> None:

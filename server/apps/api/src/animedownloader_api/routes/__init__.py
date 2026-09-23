9cd5e1f9ef25b1d01a1adb57d56062fcaf5688e9
Hennano <hennanoyo@gmail.com>
2026-09-23 21:23:08 +0900

    feat: register media processing routes

--- server/apps/api/src/animedownloader_api/routes/__init__.py
+++ server/apps/api/src/animedownloader_api/routes/__init__.py
@@ -1,11 +1,13 @@
 from .animes import router as animes_router
 from .download_jobs import router as download_jobs_router
 from .episodes import router as episodes_router
+from .media_processing_jobs import router as media_processing_jobs_router
 from .releases import router as releases_router
 
 __all__ = [
     "animes_router",
     "download_jobs_router",
     "episodes_router",
+    "media_processing_jobs_router",
     "releases_router",
 ]

553dec06b312f10147373d3fea9d39ea0ead18d6
Hennano <hennanoyo@gmail.com>
2026-09-24 01:04:44 +0900

    feat: expose subtitle processing contract

--- server/domains/media-asset/src/animedownloader_media_asset/__init__.py
+++ server/domains/media-asset/src/animedownloader_media_asset/__init__.py
@@ -1,13 +1,18 @@
+from .enums import SubtitleTrackStatus
 from .exceptions import MediaAssetValidationError
 from .metadata import MediaAssetMetadata, SubtitleTrackMetadata
 from .models import MediaAsset, SubtitleTrack
 from .service import MediaAssetService
+
+SUBTITLE_PROCESSING_TASK_NAME = "animedownloader.process-subtitle-tracks"
 
 __all__ = [
     "MediaAsset",
     "MediaAssetMetadata",
     "MediaAssetService",
     "MediaAssetValidationError",
+    "SUBTITLE_PROCESSING_TASK_NAME",
     "SubtitleTrack",
     "SubtitleTrackMetadata",
+    "SubtitleTrackStatus",
 ]

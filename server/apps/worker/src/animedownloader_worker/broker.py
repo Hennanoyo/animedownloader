b71439ada9d66390ac50796e7fec91b9bafa48b3
Hennano <hennanoyo@gmail.com>
2026-09-24 09:58:03 +0900

    fix: harden worker Redis connection timeouts

--- server/apps/worker/src/animedownloader_worker/broker.py
+++ server/apps/worker/src/animedownloader_worker/broker.py
@@ -6,6 +6,10 @@ settings = Settings()
 
 broker: AsyncBroker = RedisStreamBroker(
     url=settings.redis_url,
+    xread_block=2000,
+    socket_connect_timeout=5.0,
+    socket_timeout=10.0,
+    unacknowledged_lock_timeout=30.0,
 )
 
 

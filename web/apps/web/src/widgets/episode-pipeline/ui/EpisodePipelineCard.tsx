ca689246774d1b92e17264307612ed2362a0110a
Hennano <hennanoyo@gmail.com>
2026-09-25 05:13:38 +0900

    fix(web): format pipeline status labels

--- web/apps/web/src/widgets/episode-pipeline/ui/EpisodePipelineCard.tsx
+++ web/apps/web/src/widgets/episode-pipeline/ui/EpisodePipelineCard.tsx
@@ -141,7 +141,7 @@ function PipelineStage({ label, status, detail }: PipelineStageProps) {
 function formatStatus(status: PipelineStageStatus): string {
   return status
     .replaceAll("_", " ")
-    .replace(/\bw/g, (letter) => letter.toUpperCase());
+    .replace(/\b\w/g, (letter) => letter.toUpperCase());
 }
 
 function formatDownloadProgress(download: {

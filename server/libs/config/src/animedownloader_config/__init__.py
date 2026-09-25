2c9b512c983e0d796670205a099258fc4e8acdc6
Hennano <hennanoyo@gmail.com>
2026-09-25 20:26:38 +0900

    feat(config): export media progress stage type

--- server/libs/config/src/animedownloader_config/__init__.py
+++ server/libs/config/src/animedownloader_config/__init__.py
@@ -1,9 +1,15 @@
-from .job_events import JOB_PROGRESS_CHANNEL, JobProgressEvent, JobProgressReadyEvent
+from .job_events import (
+    JOB_PROGRESS_CHANNEL,
+    JobProgressEvent,
+    JobProgressReadyEvent,
+    JobProgressStage,
+)
 from .settings import Settings
 
 __all__ = [
     "JOB_PROGRESS_CHANNEL",
     "JobProgressEvent",
     "JobProgressReadyEvent",
+    "JobProgressStage",
     "Settings",
 ]

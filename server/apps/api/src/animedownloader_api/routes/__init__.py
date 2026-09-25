0d45b4340b1cc4dca094a9022b2f441e676a3d68
Hennano <hennanoyo@gmail.com>
2026-09-25 09:48:33 +0900

    fix: organize job event router imports

--- server/apps/api/src/animedownloader_api/routes/__init__.py
+++ server/apps/api/src/animedownloader_api/routes/__init__.py
@@ -3,8 +3,8 @@ from .animes import router as animes_router
 from .download_jobs import router as download_jobs_router
 from .episode_pipeline import router as episode_pipeline_router
 from .episodes import router as episodes_router
-from .media_packaging_jobs import router as media_packaging_jobs_router
 from .job_events import router as job_events_router
+from .media_packaging_jobs import router as media_packaging_jobs_router
 from .media_preparation_jobs import router as media_preparation_jobs_router
 from .media_processing_jobs import router as media_processing_jobs_router
 from .releases import router as releases_router

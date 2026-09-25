2b27bcff9889ead1ec490aff7207836497a50680
Hennano <hennanoyo@gmail.com>
2026-09-25 09:07:37 +0900

    fix(web): wire download job cards into manager page

--- web/apps/web/src/pages/downloads/ui/DownloadManagerPage.tsx
+++ web/apps/web/src/pages/downloads/ui/DownloadManagerPage.tsx
@@ -1,4 +1,5 @@
 import { Link, useSearch } from "@tanstack/react-router";
+import DownloadJobCard from "../../../features/download-management/ui/DownloadJobCard";
 import { Button } from "react-aria-components";
 import {
   activeDownloadStatuses,

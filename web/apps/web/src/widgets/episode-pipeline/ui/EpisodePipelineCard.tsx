d465a7fe19905ec03cffbde5fabdd26894d18af5
Hennano <hennanoyo@gmail.com>
2026-09-26 17:39:54 +0900

    feat: expose source recovery from episode pipeline

--- web/apps/web/src/widgets/episode-pipeline/ui/EpisodePipelineCard.tsx
+++ web/apps/web/src/widgets/episode-pipeline/ui/EpisodePipelineCard.tsx
@@ -21,6 +21,7 @@ import {
 import EpisodeDownloadControl from "../../../features/episode-download/ui/EpisodeDownloadControl";
 import { useDeleteEpisode } from "../../../features/episode-management/model/useEpisodeManagement";
 import { useRetryEpisodePipeline } from "../../../features/anime-detail/model/useAnimePipeline";
+import MediaSourceRecovery from "../../../features/media-source-recovery/ui/MediaSourceRecovery";
 import Icon from "../../../shared/ui/Icon";
 import styles from "./EpisodePipelineCard.module.scss";
 
@@ -487,8 +488,9 @@ function renderStageBody(
 
     if (stageStatus === "completed") {
       return (
-        <OutputStatusList
-          items={[
+        <>
+          <OutputStatusList
+            items={[
             {
               label: "File downloaded",
               detail: formatDownloadedBytes(

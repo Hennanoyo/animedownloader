7aa55ffb56b1c909ca29a910a6c3bb888f8da3eb
Hennano <hennanoyo@gmail.com>
2026-09-27 00:31:14 +0900

    feat: expose Nyaa RSS result limit

--- server/libs/nyaa/src/animedownloader_nyaa/__init__.py
+++ server/libs/nyaa/src/animedownloader_nyaa/__init__.py
@@ -1,3 +1,9 @@
-from .client import NyaaClient, NyaaError, NyaaParseError, NyaaUpstreamError
+from .client import NyaaClient, NyaaError, NyaaParseError, NyaaUpstreamError, RSS_RESULT_LIMIT
 
-__all__ = ["NyaaClient", "NyaaError", "NyaaParseError", "NyaaUpstreamError"]
+__all__ = [
+    "NyaaClient",
+    "NyaaError",
+    "NyaaParseError",
+    "NyaaUpstreamError",
+    "RSS_RESULT_LIMIT",
+]
